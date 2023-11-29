f67ef557394ef137260f24695522ee56a1b71a37
Matheus <bigmath00@gmail.com>
2023-11-29 17:55:37 -0300

    style (responsividade): adiciona breakpoints de responsividade

--- src/App.vue
+++ src/App.vue
@@ -106,7 +106,7 @@ watch(darkMode, (mode) => {
       },
       detectRetina: true
     }" />
-    <header class="absolute right-0 z-10 px-7 py-8">
+    <header class="absolute right-0 z-10 px-7 py-8 hidden lg:block">
       <Navbar>
         <li class="cursor-pointer" :class="darkMode ? 'hover:text-[#ff8ff5]' : 'hover:text-gray-200'">Início</li>
         <li class="cursor-pointer" :class="darkMode ? 'hover:text-[#ff8ff5]' : 'hover:text-gray-200'">Projetos</li>
@@ -134,10 +134,10 @@ watch(darkMode, (mode) => {
         </li>
       </Navbar>
     </header>
-  <div class="h-screen flex justify-center flex-col relative">
-    <main class="flex justify-center px-7 py-8">
-      <section class="w-1/4 h-1/4 flex flex-col gap-7">
-        <h1 class="text-4xl	font-extrabold leading-10">Matheus Santos</h1>
+  <div class="h-screen flex lg:justify-center items-center lg:flex-col relative">
+    <main class="flex justify-center lg:px-7 lg:py-8 ">
+      <section class="flex flex-col text-center lg:text-left max-w-[80%] lg:max-w-[50%] 2xl:max-w-[35%] justify-center text-sm lg:text-base gap-3 lg:gap-9 leading-loose">
+        <h1 class="text-xl lg:text-4xl font-extrabold lg:leading-10">Matheus Santos</h1>
         <p>Olá, seja bem vindo(a) ao meu portifólio.</p>
         <p>Sou um Desenvolvedor Front-end com expertise em transformar conceitos criativos em experiências web.
           Meu foco principal é a utilização da linguagem JavaScript junto ao seus frameworks, Vue.js e Nuxt.js para criar
@@ -148,27 +148,31 @@ watch(darkMode, (mode) => {
           simplicidade e valorizar as conexões com as pessoas.</p>
         <p>Me encontre nas redes</p>
         <div>
-          <ul class="flex gap-3">
+          <ul class="flex lg:justify-start justify-center gap-3">
             <li>
               <a class="flex items-center gap-1 cursor-pointer" :class="darkMode ? 'hover:text-[#ff8ff5]' : 'hover:text-gray-200'"
                 href="https://www.linkedin.com/in/bigmath/">
-                <Icon width="25" height="25" icon="mdi:linkedin" />Linkedin
+                <Icon width="25" height="25" icon="mdi:linkedin" />
+                <span class="hidden lg:block">Linkedin</span>
               </a>
             </li>
             <li>
               <a class="flex items-center gap-1 cursor-pointer" :class="darkMode ? 'hover:text-[#ff8ff5]' : 'hover:text-gray-200'" href="https://github.com/bigmathdev">
-                <Icon width="25" height="25" icon="mdi:github" />GitHub
+                <Icon width="25" height="25" icon="mdi:github" />
+                <span class="hidden lg:block">GitHub</span>
               </a>
             </li>
             <li>
               <a class="flex items-center gap-1 cursor-pointer" :class="darkMode ? 'hover:text-[#ff8ff5]' : 'hover:text-gray-200'"
                 href="https://www.instagram.com/bigmath.dev">
-                <Icon width="25" height="25" icon="mdi:instagram" />Instagram
+                <Icon width="25" height="25" icon="mdi:instagram" />
+                <span class="hidden lg:block">Instagram</span>
               </a>
             </li>
             <li>
               <a class="flex items-center gap-1 cursor-pointer" :class="darkMode ? 'hover:text-[#ff8ff5]' : 'hover:text-gray-200'" href="mailto:contato@bigmath.dev">
-                <Icon width="25" height="25" icon="mdi:email-outline" />E-mail
+                <Icon width="25" height="25" icon="mdi:email-outline" />
+                <span class="hidden lg:block">E-mail</span>
               </a>
             </li>
           </ul>
@@ -182,7 +186,7 @@ watch(darkMode, (mode) => {
 
 <style>
 body {
-  @apply font-inter leading-7;
+  @apply font-inter lg:leading-7;
   transition: ease-in-out 1000ms
 }
 
