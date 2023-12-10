86ae12ef33de5f493a8d448caba99349aa0e400c
MATHEUS-VM\bigma <bigmath00@gmail.com>
2023-12-10 18:03:39 -0300

    feat (projetos): adiciona projetos

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -2,8 +2,20 @@
 
 <template>
   <div>
-    <h1>Projetos</h1>
-    <span>Store</span>
+    <h1>Projetos listados em breve</h1>
+
+    <ul>
+      <li><a href="https://bigmathdev.github.io/treine-me/">Site estático</a></li>
+      <li><a href="https://bigmathdev.github.io/space-cream-responsive-element/">Space Cream - Site responsivo</a></li>
+      <li><a href="https://bigmathdev.github.io/responsive-element/">Trabalhos - Mobile first</a></li>
+      <li><a href="https://bigmathdev.github.io/prototype-weather-api/">Protótipo APP Clima</a></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
