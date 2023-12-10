7f64eebf40b633909731f7ce813b449055ffcfa1
MATHEUS-VM\bigma <bigmath00@gmail.com>
2023-12-10 14:18:27 -0300

    refactor (componentes): formata componentes

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,22 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from "vue-router";
 
 const routes = [
-
   {
-  path: '/',
-  name: 'HomeView',
-  component: () => import('../views/HomeView.vue'),
-  // props: {
-  //   darkMode: true
-  // }
+    path: "/",
+    name: "HomeView",
+    component: () => import("../views/HomeView.vue"),
   },
 
   {
-  path: '/projetos',
-  name: 'projetos',
-  component: () => import('../views/ProjectsView.vue'),
+    path: "/projetos",
+    name: "projetos",
+    component: () => import("../views/ProjectsView.vue"),
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
