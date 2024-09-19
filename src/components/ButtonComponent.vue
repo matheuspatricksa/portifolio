21b7730507b130b3c1502c78377c13ea0891119d
bigmathdev <83350374+bigmathdev@users.noreply.github.com>
2024-09-19 23:48:35 +0000

    Build: (454b296) Alterna nome do workflow

--- src/components/ButtonComponent.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <button :type="props.type" class="rounded-md bg-[#7480ff] p-3 text-white shadow-2xl hover:bg-[#646ee4]">{{ placeholder }}</button>
-</template>
-
-<script setup>
-const props = defineProps(['type', 'placeholder'])
-</script>
